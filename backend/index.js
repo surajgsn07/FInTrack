b689e2dc4eee544b87939effd51e216757eb3730
surajgsn07 <surajgn07@gmail.com>
2025-03-05 14:03:46 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin : "*",
+    origin : "https://fin-track-gsn.netlify.app",
     methods:['GET' , 'POST' , 'PUT' , 'DELETE'],
     credentials:true
 }))
